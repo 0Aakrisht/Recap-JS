2e25cad6975c587a80db61cc92149b07174f5240
0Aakrisht <paakrisht123@gmail.com>
2025-11-15 17:35:07 +0545

    first file done

---
+++ file.1/main.js
@@ -0,0 +1,9 @@
+// 1. Convert string "23" to a number and add 10
+let first = "23";
+let change = Number(first);
+
+let second = 10 ;
+
+let add = change + second ;
+
+console.log(add);
---
+++ main.js
@@ -0,0 +1,13 @@
+let a = 5 ;
+let b = "6" ;
+
+let string = String(a);
+let number = Number(b);
+
+console.log(typeof(a));
+console.log(typeof(string));
+console.log(typeof(b));
+console.log(typeof(number));
+
+
+
