d46a4fabf6dc222069618c95be699d11d04d3160
0Aakrisht <paakrisht123@gmail.com>
2025-11-15 19:27:45 +0545

    ternary also done

---
+++ file.7/main.js
@@ -0,0 +1,6 @@
+// 7.Use ternary operator to check if a number is even or odd
+
+let number = prompt("Enter a number \n");
+
+number > 0 ? console.log("even") : console.log("odd");
+ 
---
+++ file.8/main.js
@@ -0,0 +1,5 @@
+// 8. Check if a user is allowed access:
+
+// age ≥ 18
+
+// OR has parental permission
