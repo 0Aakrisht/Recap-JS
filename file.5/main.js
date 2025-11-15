f241a4f0047275a0d2ca727e37ba4a34bd51f45c
0Aakrisht <paakrisht123@gmail.com>
2025-11-15 18:44:21 +0545

    fifth file is also done

---
+++ file.5/main.js
@@ -0,0 +1,23 @@
+// 5. Create a “grading system” using if/else
+
+let marks = prompt("Enter your marks \n");
+
+if(marks >= 80)
+{
+    console.log("Distinction");
+}
+else if (marks >=70)
+{
+    console.log("First divison");
+}
+else if (marks >=60)
+{
+    console.log("Second divison");
+}
+else if( marks >= 50)
+{
+    console.log("Third divison");
+}
+else {
+    console.log("Failed");
+}
