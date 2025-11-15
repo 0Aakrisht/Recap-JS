d69349ad858c093bf753d8e74a92fbc9564a7012
0Aakrisht <paakrisht123@gmail.com>
2025-11-15 18:32:51 +0545

    second file is also done

---
+++ file.2/main.js
@@ -0,0 +1,17 @@
+// 2. Check if a number is positive, negative, or zero
+
+let number = prompt("Enter a number \n");
+
+if(number > 0)
+{
+    console.log("it is positive");
+}
+else if (number < 0)
+{
+console.log("it is negative");
+
+}
+else
+{
+    console.log("it is a zero");
+}
