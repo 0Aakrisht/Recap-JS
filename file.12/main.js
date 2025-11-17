8ad0dc24f15b260d2f072ba55f7ef01acd19d012
0Aakrisht <paakrisht123@gmail.com>
2025-11-17 19:00:43 +0545

    reversed the string

--- file.12/main.js
+++ file.12/main.js
@@ -1,2 +1,10 @@
-// String Reverser
-// Reverse any string using a loop.
+    // String Reverser
+    // Reverse any string using a loop.
+    let array = "Aakrisht";
+    let rev = "";
+
+    for(i = array.length - 1 ; i >= 0 ; i--)
+    {
+        rev += rev
+    }
+    console.log(rev);
